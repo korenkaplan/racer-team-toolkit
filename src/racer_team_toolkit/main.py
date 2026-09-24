d94cdd18e0da76e69899d4ecf7bdb8bbd46716fc
korenkaplan <99595036+korenkaplan@users.noreply.github.com>
2026-09-24 16:08:48 +0300

    Route main menu to Ronen operations

--- src/racer_team_toolkit/main.py
+++ src/racer_team_toolkit/main.py
@@ -3,10 +3,7 @@ from racer_team_toolkit.config import TOOL_MENU_CHOICES
 from racer_team_toolkit.full_release_update.main import (
     main as run_full_release_update_main,
 )
-from racer_team_toolkit.imu_recordings.main import (
-    main as run_imu_recordings_main,
-)
-from racer_team_toolkit.jar_management.main import main as run_jar_management_main
+from racer_team_toolkit.jar_management.main import main as run_ronen_operations_main
 from racer_team_toolkit.quick_reset.main import main as run_quick_reset_main
 from racer_team_toolkit.reff_extractor.main import (
     main as run_reff_and_videos_extractor_main,
@@ -38,15 +35,12 @@ def main() -> None:
             run_apk_installer_main()
 
         elif choice == TOOL_MENU_CHOICES[3]:
-            run_jar_management_main()
+            run_ronen_operations_main()
 
         elif choice == TOOL_MENU_CHOICES[4]:
             run_quick_reset_main()
 
-        elif choice == TOOL_MENU_CHOICES[5]:
-            run_imu_recordings_main()
-
-        elif choice == TOOL_MENU_CHOICES[6] or choice is None:
+        elif choice == TOOL_MENU_CHOICES[5] or choice is None:
             break
 
 
