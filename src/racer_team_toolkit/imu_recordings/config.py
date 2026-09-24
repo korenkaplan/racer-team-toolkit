b984476c034e2a84b325cce60cc6a9276b378b20
korenkaplan <99595036+korenkaplan@users.noreply.github.com>
2026-09-24 09:55:47 +0300

    Fix IMU recordings remote directory

--- src/racer_team_toolkit/imu_recordings/config.py
+++ src/racer_team_toolkit/imu_recordings/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-IMU_REMOTE_DIRECTORY = "/home/pod/utils/imuRecord"
+IMU_REMOTE_DIRECTORY = "/home/pod/imuRecord"
 IMU_FILE_PREFIX = "imu_"
 IMU_FILE_SUFFIX = ".csv"
